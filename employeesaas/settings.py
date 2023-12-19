0b09e80a15606442bd8a36be252b2f3a10a23096
Girish C <63511244+girishc24@users.noreply.github.com>
2023-12-19 17:16:25 +0530

    Update settings.py

--- employeesaas/settings.py
+++ employeesaas/settings.py
@@ -73,6 +73,17 @@ WSGI_APPLICATION = 'employeesaas.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+'''DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_database_name',
+        'USER': 'your_database_user',
+        'PASSWORD': 'your_database_password',
+        'HOST': 'localhost',  
+        'PORT': '5432',      
+    }
+}'''
+
 
 DATABASES = {
     'default': {
